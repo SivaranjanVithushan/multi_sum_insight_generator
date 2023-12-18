bd701faa1ac5c428b17b6bca6294fd17ea9c22ef
Sivaranjan Vithushan <ict19868@fot.sjp.ac.lk>
2023-12-18 14:32:56 +0530

    Refactor Aboutus.css and Index.jsx files

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,22 +1,105 @@
+// import React, { useState } from "react";
+// import '../Styles/Signin.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+// import { useNavigate } from "react-router-dom";
+// import logo from '../img/logo.png';
+
+// function Signin() {
+
+//     // let [passwordVisible, setPasswordVisible] = useState(false);
+//     let [password, setPassword] = useState('');
+//     let [email, setEmail] = useState('');
+
+//     // function togglePasswordVisibility() {
+//     //     setPasswordVisible(!passwordVisible);
+//     // };
+
+
+//     function handleSubmit() {
+//         console.log('Email:', email);
+//         console.log('Password:', password);
+//     };
+
+//     const navigate = useNavigate();
+
+//     const routeChange = () => {
+//         let path = `/register`;
+//         navigate(path)
+//     }
+
+//     return (
+//         <div className="sign_container">
+//             <div className="left-container">
+//                 <div className="logo_container">
+//                     <img src={logo} alt="logo" className="logo" />
+//                 </div>
+//                 <h3 className="para_1">New here?</h3>
+//                 <p className="para_2">Sign up and discover a great amount of new opportunities!</p>
+//                 <button className="register_btn" onClick={routeChange}>SIGN UP</button>
+//             </div>
+//             <div className="right-container">
+//                 <h3 className="sign_in"> Sign In</h3>
+//                 {/* <form> */}
+//                 <div className="form_container">
+//                     <div className="input_container">
+//                         <label htmlFor="email"> 
+//                             <FontAwesomeIcon icon={faEnvelope} className="icon" />
+//                         </label>
+
+//                         <input
+//                             type="email"
+//                             name="email"
+//                             id="email"
+//                             placeholder="Your email"
+//                             onChange={(e) => { setEmail(e.target.value) }}
+//                             required
+//                         />
+//                         {/* <div className="icon_container">
+//                             <FontAwesomeIcon icon={faEnvelope} className="envelope-icon" />
+//                             <input className="email_container" type="email" name="email" id="email" placeholder="Your email" onChange={(e) => { setEmail(e.target.value) }} required />
+//                         </div> */}
+//                     </div>
+
+//                     <div className="input_container">
+//                         <label htmlFor="password">
+//                             <FontAwesomeIcon icon={faLock} className="icon" />
+//                         </label>
+//                         <input
+//                             type="password"
+//                             name="password"
+//                             id="password"
+//                             placeholder="Your password"
+//                             onChange={(e) => { setPassword(e.target.value) }}
+//                             required
+//                         />
+//                     </div>
+//                 </div>
+
+//                 <button type="submit" className="submit_btn" onClick={handleSubmit}>SIGN IN</button>
+//                 {/* </form> */}
+//             </div>
+//         </div>
+//     );
+// }
+
+
+// export default Signin;
+
+// Signin.js
+
 import React, { useState } from "react";
 import '../Styles/Signin.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faLock} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom";
+import logo from '../img/logo.png';
 
 function Signin() {
-
-    // let [passwordVisible, setPasswordVisible] = useState(false);
     let [password, setPassword] = useState('');
     let [email, setEmail] = useState('');
 
-    // function togglePasswordVisibility() {
-    //     setPasswordVisible(!passwordVisible);
-    // };
-
-
     function handleSubmit() {
-
         console.log('Email:', email);
         console.log('Password:', password);
     };
@@ -25,57 +108,56 @@ function Signin() {
 
     const routeChange = () => {
         let path = `/register`;
-        navigate(path)
+        navigate(path);
     }
 
     return (
         <div className="sign_container">
-            <button className="sign_logo_btn">Logo</button>
             <div className="left-container">
+                <div className="logo_container">
+                    <img src={logo} alt="logo" className="logo" />
+                </div>
                 <h3 className="para_1">New here?</h3>
-                <p className="para_2">Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                <button className="register_btn" onClick={routeChange}>REGISTER</button>
+                <p className="para_2">Sign up and discover a great amount of new opportunities!</p>
+                <button className="register_btn" onClick={routeChange}>SIGN UP</button>
             </div>
             <div className="right-container">
                 <h3 className="sign_in"> Sign In</h3>
-                {/* <form> */}
-                    <div className="form_container">
-                        <div className="email_content">
-                            <label htmlFor="email">Email</label>
-                            <br />
-                            <div className="icon_container">
-                                <FontAwesomeIcon icon={faEnvelope} className="envelope-icon" />
-                                <input className="email_container" type="email" name="email" id="email" placeholder="Your email" onChange={(e)=>{setEmail(e.target.value)}} required />
-                            </div>
-                        </div>
-                        <div className="password_content">
-                            <label htmlFor="password">Password</label>
-                            <br />
-                            <div className="icon_container">
-                                <FontAwesomeIcon icon={faLock} className="lock-icon" />
-                                <input className="password_container"
-                                    type='password'
-                                    name="password"
-                                    id="password"
-                                    placeholder="Your password"
-                                    onChange={(e)=>{setPassword(e.target.value)}}                                    // onChange={this.handlePasswordChange}
-                                    required
-                                />
-                                {/* <FontAwesomeIcon
-                                    icon={passwordVisible ? faEye : faEyeSlash}
-                                    className="eye-icon"
-                                    onClick={togglePasswordVisibility}
-                                /> */}
-                            </div>
-                        </div>
+                <div className="form_container">
+                    <div className="input_container">
+                        <label htmlFor="email">
+                            <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Your email"
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            required
+                        />
                     </div>
 
-                    <button type="submit" className="submit_btn" onClick={handleSubmit}>SIGN IN</button>
-                {/* </form> */}
+                    <div className="input_container">
+                        <label htmlFor="password">
+                            <FontAwesomeIcon icon={faLock} className="icon" />
+                        </label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Your password"
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            required
+                        />
+                    </div>
+                </div>
+
+                <button type="submit" className="submit_btn" onClick={handleSubmit}>SIGN IN</button>
             </div>
         </div>
     );
 }
 
-
 export default Signin;
+
