3b5f2d9c97d838ca707126eb0e70ff6a2fd3f830
Mohamed Safras <mohamed.safras@acentura.com>
2024-02-10 07:14:34 +0530

    home statistics card has been added

--- src/constants/navlinks.ts
+++ src/constants/navlinks.ts
@@ -1,6 +1,6 @@
 
 export const navLinks = [
-    { id: 1, href: "/", title: "Home" },
+    { id: 1, href: "home", title: "Home" },
     { id: 3, href: "about", title: "About" },
     { id: 2, href: "contact", title: "Contact" },
     { id: 4, href: "logout", title: "Logout" },
