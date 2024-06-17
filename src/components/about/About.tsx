5ebbd2360cdb2c4fd5016d133315c6a3d9760d50
Sivaranjan Vithushan <ict19868@fot.sjp.ac.lk>
2024-06-18 00:51:35 +0530

    Refactor About component to improve code formatting and add missing semicolon

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,15 +1,16 @@
 // About.tsx
 import React from 'react';
-import Members from './Members'
+import Members from './Members';
 
 const About: React.FC = () => {
   return (
     <div>
-      <Members/>
-      <div>
-        <h3 className="text-2xl font-bold text-white text-center mt-10">MULTILINGUAL DATA SUMMARIZATION AND INSIGHTS GENERATION FOR DECISION-MAKING IN GROCERY RETAIL</h3>
+      <Members />
+      <div className="mt-10">
+        <h3 className="text-2xl font-bold text-white text-center">
+          MULTILINGUAL DATA SUMMARIZATION AND INSIGHTS GENERATION FOR DECISION-MAKING IN GROCERY RETAIL
+        </h3>
       </div>
-    
     </div>
   );
 };
